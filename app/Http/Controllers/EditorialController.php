c371b234039abe968343482747aee8bcbd558071
Silvino <109769461+JarvisSAV@users.noreply.github.com>
2024-10-06 23:48:05 +0000

    generación de alta

---
+++ app/Http/Controllers/EditorialController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Editorial;
+use Illuminate\Http\Request;
+
+class EditorialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('editorial.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'nombre' => 'required|min:5|max:100',
+            'domicilio' => 'required|min:5|max:100',
+            'correo' => 'required|email',
+        ]);
+
+        $editorial = new Editorial();
+        $editorial->nombre = $request->nombre;
+        $editorial->domicilio = $request->domicilio;
+        $editorial->correo = $request->correo;
+        $editorial->save();
+
+        return redirect()->route('editoriales.index')->with('message', 'Editorial creada con éxito');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
